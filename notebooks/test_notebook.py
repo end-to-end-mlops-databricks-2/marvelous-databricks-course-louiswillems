9f017be8970309e725d0959b533732d51bdb69b9
Louis Willems <louis.willems@outlook.com>
2025-02-02 18:36:06 -0500

    second commit

---
+++ notebooks/test_notebook.py
@@ -0,0 +1,85 @@
+# Databricks notebook source
+
+# MAGIC %md
+# MAGIC # Hotel Reservation Prediction Exercise
+# MAGIC
+# MAGIC This notebook demonstrates how to predict Hotel Reservation Status using the Hotel Reservation dataset. We'll go through the process of loading data, preprocessing, model creation, and visualization of results.
+# MAGIC
+# MAGIC ## Importing Required Libraries
+# MAGIC
+# MAGIC First, let's import all the necessary libraries.
+
+# COMMAND ----------
+import pandas as pd
+import yaml
+from sklearn.model_selection import train_test_split
+
+# COMMAND ----------
+
+# Only works in a Databricks environment if the data is there
+filepath = "/Volumes/mlops_dev/louiswil/wine_quality_data/wine-quality-white-and-red.csv"
+# Load the data
+df = pd.read_csv(filepath)
+
+# Works both locally and in a Databricks environment
+filepath = "../data/wine-quality-white-and-red.csv"
+# Load the data
+df = pd.read_csv(filepath)
+df.head(2)
+
+# COMMAND ----------
+
+# Load configuration
+with open("../project_config.yml", "r") as file:
+    config = yaml.safe_load(file)
+
+print(config.get('catalog_name'))
+num_features = config.get('num_features')
+print(num_features)
+
+
+# MAGIC ## Preprocessing
+
+# COMMAND ----------
+
+# Remove rows with missing target
+
+# Handle missing values and convert data types as needed
+df["fixed acidity"] = pd.to_numeric(df["fixed acidity"], errors="coerce")
+
+median_no_of_previous_cancellations = df["alcohol"].median()
+df["alcohol"].fillna(median_no_of_previous_cancellations, inplace=True)
+
+# Handle numeric features
+num_features = config.get('num_features')
+for col in num_features:
+    df[col] = pd.to_numeric(df[col], errors="coerce")
+
+# Fill missing values with mean or default values
+df.fillna(
+    {
+        "citric acid": df["citric acid"].mean(),
+        # "type_of_meal_plan": "None",
+        "sulphates": 0,
+    },
+    inplace=True,
+)
+
+# Convert categorical features to the appropriate type
+cat_features = config.get('cat_features')
+for cat_col in cat_features:
+     df[cat_col] = df[cat_col].astype("category")
+
+# Extract target and relevant features
+target = config.get('target')
+# relevant_columns = cat_features + num_features + [target]
+
+df["Id"] = range(1, len(df) + 1)
+relevant_columns = cat_features + num_features + [target] + ["Id"]
+print(relevant_columns)
+
+df = df[relevant_columns]
+df["Id"] = df["Id"].astype("str")
+train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+df.head(2)
---
+++ src/wine_quality/data_processor.py
@@ -0,0 +1,84 @@
+import pandas as pd
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import current_timestamp, to_utc_timestamp
+from sklearn.model_selection import train_test_split
+
+from wine_quality.config import ProjectConfig
+
+
+class DataProcessor:
+    def __init__(self, pandas_df: pd.DataFrame, config: ProjectConfig):
+        self.df = pandas_df  # Store the DataFrame as self.df
+        self.config = config  # Store the configuration
+
+    def preprocess(self):
+        """Preprocess the DataFrame stored in self.df"""
+        # Handle missing values and convert data types as needed
+        self.df["fixed acidity"] = pd.to_numeric(self.df["fixed acidity"], errors="coerce")
+
+        median_no_of_previous_cancellations = self.df["alcohol"].median()
+        self.df["alcohol"].fillna(median_no_of_previous_cancellations, inplace=True)
+
+
+
+        # Handle numeric features
+        num_features = self.config.num_features
+        for col in num_features:
+            self.df[col] = pd.to_numeric(self.df[col], errors="coerce")
+
+        # Fill missing values with mean or default values
+        self.df.fillna(
+            {
+                "citric acid": self.df["citric acid"].mean(),
+                # "type_of_meal_plan": "None",
+                "sulphates": 0,
+            },
+            inplace=True,
+)
+
+        # Convert categorical features to the appropriate type
+        cat_features = self.config.cat_features
+        for cat_col in cat_features:
+            self.df[cat_col] = self.df[cat_col].astype("category")
+
+        # Extract target and relevant features
+        target = self.config.target
+
+        self.df["Id"] = range(1, len(self.df) + 1)
+        relevant_columns = cat_features + num_features + [target] + ["Id"]
+        self.df = self.df[relevant_columns]
+        self.df["Id"] = self.df["Id"].astype("str")
+
+    def split_data(self, test_size=0.2, random_state=42):
+        """Split the DataFrame (self.df) into training and test sets."""
+        train_set, test_set = train_test_split(self.df, test_size=test_size, random_state=random_state)
+        return train_set, test_set
+
+    def save_to_catalog(self, train_set: pd.DataFrame, test_set: pd.DataFrame, spark: SparkSession):
+        """Save the train and test sets into Databricks tables."""
+
+        train_set_with_timestamp = spark.createDataFrame(train_set).withColumn(
+            "update_timestamp_utc", to_utc_timestamp(current_timestamp(), "UTC")
+        )
+
+        test_set_with_timestamp = spark.createDataFrame(test_set).withColumn(
+            "update_timestamp_utc", to_utc_timestamp(current_timestamp(), "UTC")
+        )
+
+        train_set_with_timestamp.write.mode("overwrite").saveAsTable(
+            f"{self.config.catalog_name}.{self.config.schema_name}.train_set"
+        )
+
+        test_set_with_timestamp.write.mode("overwrite").saveAsTable(
+            f"{self.config.catalog_name}.{self.config.schema_name}.test_set"
+        )
+
+        spark.sql(
+            f"ALTER TABLE {self.config.catalog_name}.{self.config.schema_name}.train_set "
+            "SET TBLPROPERTIES (delta.enableChangeDataFeed = true);"
+        )
+
+        spark.sql(
+            f"ALTER TABLE {self.config.catalog_name}.{self.config.schema_name}.test_set "
+            "SET TBLPROPERTIES (delta.enableChangeDataFeed = true);"
+        )
